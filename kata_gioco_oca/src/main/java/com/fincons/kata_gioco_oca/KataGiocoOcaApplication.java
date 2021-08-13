6671f5fe58a2f879133cad50a4b6766febea047c
francesco.violante <francesco.violante@finconsgroup.com>
2021-08-13 14:21:51 +0200

    initialize project OCA

---
+++ kata_gioco_oca/src/main/java/com/fincons/kata_gioco_oca/KataGiocoOcaApplication.java
@@ -0,0 +1,13 @@
+package com.fincons.kata_gioco_oca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataGiocoOcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataGiocoOcaApplication.class, args);
+    }
+
+}
---
+++ kata_gioco_oca/src/test/java/com/fincons/kata_gioco_oca/KataGiocoOcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincons.kata_gioco_oca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataGiocoOcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
